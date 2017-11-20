d1c18443cdf21b3830f1277443b91ca52c4b1517
halimelnagar <halim.elnagar@gmail.com>
2017-11-20 19:56:56 +0000

    change gem - sqllite for dev only

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,13 @@
 class TasksController < ApplicationController
-	
+  # before_action :set_project, only: [:show, :edit, :update, :destroy]
+
+	def index
+     @project = Project.find(params[:project_id])
+     @tasks = @project.tasks     
+  end
+
+  private
+  def set_project
+      @project = Project.find(params[:project_id])
+    end
 end
